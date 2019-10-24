8b82f9b0fc922687236ac2e85289148f47db5f90
Ted Osuch <ted@osuch.com>
2019-10-24 13:12:10 +0100

    DataLoader done

---
+++ src/main/java/com/codeclan/example/coursebookingservice/components/DataLoader.java
@@ -0,0 +1,48 @@
+package com.codeclan.example.coursebookingservice.components;
+
+import com.codeclan.example.coursebookingservice.models.Course;
+import com.codeclan.example.coursebookingservice.models.Customer;
+import com.codeclan.example.coursebookingservice.repositories.BookingRepository;
+import com.codeclan.example.coursebookingservice.repositories.CourseRepository;
+import com.codeclan.example.coursebookingservice.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @Autowired
+    CustomerRepository customerRepsitory;
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    public DataLoader() {
+    }
+
+    public void run(ApplicationArguments args){
+        Customer customer1 = new Customer("Sarah","Halifax",32);
+        customerRepsitory.save(customer1);
+
+        Customer customer2 = new Customer("Kevin", "Frankfurt", 45);
+        customerRepsitory.save(customer2);
+
+        Course course1 = new Course("Horticulture", "Oban", 4);
+        courseRepository.save(course1);
+
+        Course course2 = new Course("Java", "London", 5);
+        courseRepository.save(course2);
+
+
+
+
+
+
+
+    }
+}
